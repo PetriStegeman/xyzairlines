d430d0a647308ac3a2cf01335e3146cb9f8787ed
Petri Stegeman <petri_sl@hotmail.com>
2018-04-03 09:58:41 +0200

    controller + db
    trying to fix kitematic

--- src/main/java/nl/xyzairlines/xyzairlines/controller/PlaneController.java
+++ src/main/java/nl/xyzairlines/xyzairlines/controller/PlaneController.java
@@ -1,5 +1,6 @@
 package nl.xyzairlines.xyzairlines.controller;
 
+import nl.xyzairlines.xyzairlines.model.Airport;
 import nl.xyzairlines.xyzairlines.model.Plane;
 import nl.xyzairlines.xyzairlines.repository.AirportRepository;
 import nl.xyzairlines.xyzairlines.repository.PlaneRepository;
@@ -20,7 +21,25 @@ public class PlaneController {
 
     @RequestMapping(value = "", method = RequestMethod.POST)
     public Plane save(@RequestBody Plane plane){
-        return planeRepository.save(plane);
+        // First make sure the airport the new plane arrived at exist.
+        Iterable<Airport> foundAirports = airportRepository.findAll();
+        Airport correctAirport = new Airport();
+        for (Airport airport: foundAirports) {
+            if(airport.getName() == plane.getAirport().getName()) {
+                // Airport exists in db.
+                correctAirport  = airport;
+            }
+        }
+
+        // If airport exists, save the plane with the airport from the DB.
+        if(correctAirport.getName().length() > 0){
+            plane.setAirport(correctAirport);
+            return planeRepository.save(plane);
+        }
+        else {
+            // No existing writer, throw error.
+            throw new RuntimeException();
+        }
     }
 
     @RequestMapping(value = "", method = RequestMethod.GET)
